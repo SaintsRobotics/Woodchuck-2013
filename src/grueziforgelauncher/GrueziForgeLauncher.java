129fd5c844b20ff3559a65c017036e39f45c8a8a
Liam Steckler <liam@bricin.net>
2012-12-27 09:43:49 -0800

    First Commit

---
+++ src/grueziforgelauncher/GrueziForgeLauncher.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grueziforgelauncher;
+
+/**
+ *
+ * @author liam_000
+ */
+public class GrueziForgeLauncher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
