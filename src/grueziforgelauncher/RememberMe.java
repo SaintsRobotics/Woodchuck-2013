f7e303b8cf8a009b0036a686e815ed729f39c180
Liam Steckler <liam@bricin.net>
2013-01-16 10:07:30 -0800

    Rewriting "Remember Me" code

---
+++ src/grueziforgelauncher/RememberMe.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grueziforgelauncher;
+
+import java.io.File;
+
+/**
+ *
+ * @author liam_000
+ */
+class RememberMe {
+
+    static void saveAccount(String pwd, String usr, File file) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
