6d60ba06dc24aafbad5391b36ec45f039a02b555
huadianz <huadianz@huadianz-PC>
2013-02-11 16:47:52 -0800

    I frigging hate git

--- src/com/saintsrobotics/frc/Shooter.java
+++ src/com/saintsrobotics/frc/Shooter.java
@@ -1,13 +1,21 @@
 package com.saintsrobotics.frc;
 
+import edu.wpi.first.wpilibj.Relay;
+
 /**
  * The shooter for the robot.
  * @author Saints Robotics
  */
 public class Shooter {
+    
+    private final int FEEDER_RELAY_CHANNEL = 1;
+    
     private Vision vision;
+    
+    private Relay feeder;
     
     public Shooter(Vision vision) {
         this.vision = vision;
+        feeder = new Relay(FEEDER_RELAY_CHANNEL);
     }
 }
